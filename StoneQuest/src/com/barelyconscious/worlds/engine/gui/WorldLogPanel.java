17a9a39ddc24ee72c3f0b59e9784b5464136afca
matt <mattschwartz@users.noreply.github.com>
2023-12-16 17:13:04 -0600

    log alerts you of new logs when hidden

--- StoneQuest/src/com/barelyconscious/worlds/engine/gui/WorldLogPanel.java
+++ StoneQuest/src/com/barelyconscious/worlds/engine/gui/WorldLogPanel.java
@@ -1,6 +1,8 @@
 package com.barelyconscious.worlds.engine.gui;
 
+import com.barelyconscious.worlds.engine.EventArgs;
 import com.barelyconscious.worlds.engine.graphics.FontContext;
+import com.barelyconscious.worlds.engine.graphics.RenderContext;
 import com.barelyconscious.worlds.engine.gui.widgets.BackgroundPanelWidget;
 import com.barelyconscious.worlds.engine.gui.widgets.ButtonWidget;
 import com.barelyconscious.worlds.engine.gui.widgets.TextFieldWidget;
@@ -18,6 +20,10 @@ public class WorldLogPanel extends Widget {
         .anchor(0.5, 1, -350, -324)
         .size(0, 0, 700, 160)
         .build(), "Test1\nTest2\nTest3\nTest4\nTexzt5");
+    private final TextFieldWidget notificationsWidget = new TextFieldWidget(LayoutData.builder()
+        .anchor(0.5, 1, -475, -10)
+        .size(0, 0, 100, 30)
+        .build(), "0 new messages");
 
     public WorldLogPanel() {
         super(LayoutData.builder()
@@ -30,24 +36,47 @@ public class WorldLogPanel extends Widget {
         logWidget.setTextAlignment(FontContext.TextAlign.LEFT);
         logWidget.setVerticalTextAlignment(FontContext.VerticalTextAlignment.BOTTOM);
 
+        notificationsWidget.setTextAlignment(FontContext.TextAlign.LEFT);
+        notificationsWidget.setVerticalTextAlignment(FontContext.VerticalTextAlignment.TOP);
+        notificationsWidget.setEnabled(false);
 
         addWidget(container);
         addWidget(new ButtonWidget(LayoutData.builder()
-            .anchor(0.5, 1, -450, -5)
+            .anchor(0.5, 1, -475, 0)
             .size(0, 0, 100, 30)
             .build(), "Toggle Log",
             () -> {
                 container.setEnabled(!container.isEnabled());
+                if (container.isEnabled()) {
+                    numNotifications = 0;
+                }
                 return null;
             }));
-
+        addWidget(notificationsWidget);
     }
 
     // todo implement scrolling
     private int scrollLinesOffset = 0;
 
+    private int numNotifications = 0;
+
+    @Override
+    protected void onRender(EventArgs eventArgs, RenderContext renderContext) {
+        if (numNotifications > 0) {
+            notificationsWidget.setText("New messages");
+            notificationsWidget.setEnabled(true);
+        } else {
+            notificationsWidget.setEnabled(false);
+        }
+    }
+
     // todo - support formatting/colors within individual log entries
     public void addLogEntry(String entry) {
+        // add a little alert to let the player know that there are new messages
+        if (!container.isEnabled()) {
+            numNotifications++;
+        }
+
         logEntries.add(entry);
 
         int backOfTheLine = Math.max(logEntries.size(), 10);
